fdfbdb8c409691b46e2b84890388d67840c4be4a
Chris Eski <es_christos1@hotmail.com>
2021-12-07 15:10:11 +0200

    check email validity refactored

--- dist/script.js
+++ dist/script.js
@@ -1,24 +1,32 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
-const btn = document.querySelector('.form__btn');
 const errorIcon = document.querySelector('.form__icon-error');
 const errorText = document.querySelector('.form__text-error');
-const errorElements = [input, btn, errorIcon, errorText];
+const errorElements = [input, errorIcon, errorText];
 
 window.onload = function () {
   input.value = '';
 };
 
+const errorStatus = (status) => {
+  errorElements.forEach((el) => {
+    el.dataset.error = `${status}`;
+  });
+};
+
+const validateEmail = () => {
+  const email = input.value;
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    console.log(`Email <${email}> is not properly formated!`);
+    errorStatus(true);
+  } else {
+    alert('Email successfully submitted!');
+    errorStatus(false);
+    input.value = '';
+  }
+};
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(input.value);
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value)) {
-    input.value = '';
-    alert('Email successfully submitted!');
-  } else {
-    errorElements.forEach((el) => {
-      el.dataset.error = 'true';
-    });
-    console.log(`Email <${input.value}> is not properly formated!`);
-  }
+  validateEmail();
 });
